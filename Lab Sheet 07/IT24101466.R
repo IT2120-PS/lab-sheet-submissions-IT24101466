32a4ca0bf0fd235c4f688408204af69312bdd404
IT24101466 <it24101466@my.sliit.lk>
2025-09-16 13:23:35 +0530

    IT24101466 PS Lab 7

---
+++ Lab Sheet 07/IT24101466.R
@@ -0,0 +1,9 @@
+#1
+punif(25, min = 0, max = 40, lower.tail = TRUE) - punif(10, min = 0 , max = 40, lower.tail = TRUE)
+#2
+pexp(2, rate = 0.3334, lower.tail = TRUE)
+
+#3.1
+pnorm(130 , mean = 100, sd = 15, lower.tail = FALSE)
+#3.2
+qnorm(0.95, mean = 100, sd = 15)
